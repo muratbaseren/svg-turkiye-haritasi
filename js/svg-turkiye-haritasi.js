c87fb7a8df4154dda6c1f32a523ae74d2ff2f723
Murat Başeren <muratbaseren@hotmail.com>
2019-03-18 18:54:26 +0300

    sehir isimleri ve verilen bir değerin gösterimi eklendi.

--- js/svg-turkiye-haritasi.js
+++ js/svg-turkiye-haritasi.js
@@ -1,36 +1,72 @@
-/*! SVG Türkiye Haritası | MIT Lisans | dnomak.com */
+function getCityValue(jsondata, cname){
+    for(var i = 0; i < jsondata.values.length; i++){
+      if(jsondata.values[i].city.toLowerCase() === cname.toLowerCase()){
+        return jsondata.values[i].value;
+      }
+    }
+
+    return 0;
+}
 
 function svgturkiyeharitasi() {
   const element = document.querySelector('#svg-turkiye-haritasi');
   const info = document.querySelector('.il-isimleri');
+  const json = { values : [
+    { city:"edirne", value:"20" },
+    { city:"antalya", value:"10" },
+    { city:"ankara", value:"5" },
+    { city:"manisa", value:"3" },
+  ]};
 
-  element.addEventListener(
-    'mouseover',
-    function (event) {
-      if (event.target.tagName === 'path' && event.target.parentNode.id !== 'guney-kibris') {
-        info.innerHTML = [
-          '<div>',
-          event.target.parentNode.getAttribute('data-iladi'),
-          '</div>'
-        ].join('');
-      }
-    }
-  );
+  var pathes = element.querySelectorAll('path');
+  
+  for (var i = 0; i < pathes.length; i++) {
+    var cityg = pathes[i].parentNode;
+    var cityName = cityg.getAttribute('data-iladi');
+    
+    var dv = document.createElement("div");
+    dv.className = "il-isimleri";
+    
+    var val = getCityValue(json, cityName);
+    dv.innerHTML = "<div>" + cityName + " (" + val + ")" + "</div>";
+    
+    var rect = cityg.getBoundingClientRect();
+    var x = rect.left;
+    var y = rect.top;
 
-  element.addEventListener(
-    'mousemove',
-    function (event) {
-      info.style.top = event.pageY + 25 + 'px';
-      info.style.left = event.pageX + 'px';
-    }
-  );
+    dv.style.left = (x + parseInt(rect.width/3)) + "px";
+    dv.style.top = (y + parseInt(rect.height/3)) + "px";
 
-  element.addEventListener(
-    'mouseout',
-    function (event) {
-      info.innerHTML = '';
-    }
-  );
+    document.body.appendChild(dv);
+  }
+
+  // element.addEventListener(
+  //   'mouseover',
+  //   function (event) {
+  //     if (event.target.tagName === 'path' && event.target.parentNode.id !== 'guney-kibris') {
+  //       info.innerHTML = [
+  //         '<div>',
+  //         event.target.parentNode.getAttribute('data-iladi'),
+  //         '</div>'
+  //       ].join('');
+  //     }
+  //   }
+  // );
+
+  // element.addEventListener(
+  //   'mousemove',
+  //   function (event) {
+  //     info.style.top = event.pageY + 25 + 'px';
+  //     info.style.left = event.pageX + 'px';
+  //   }
+  // );
+
+  // element.addEventListener(
+  //   'mouseout',
+  //   function (event) {
+  //     info.innerHTML = '';
+  //   }
+  // );
 
   element.addEventListener(
     'click',
@@ -39,18 +75,13 @@ function svgturkiyeharitasi() {
         const parent = event.target.parentNode;
         const id = parent.getAttribute('id');
 
-        if (
-          id === 'guney-kibris'
-        ) {
-          return;
-        }
+        if (id === 'guney-kibris') return;
 
         window.location.href = (
-          '#'
-          + id
-          + '-'
-          + parent.getAttribute('data-plakakodu')
+          '#' + id + '-' + parent.getAttribute('data-plakakodu')
         );
+
+        console.log('#' + id + '-' + parent.getAttribute('data-plakakodu'));
       }
     }
   );
